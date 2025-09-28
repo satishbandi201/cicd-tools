1cc4cad326ce92bf5be08123d1a1cae2bced6691
satishbandi201 <bandisatish20@gmail.com>
2025-09-28 16:09:28 +0530

    cicd-tools

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "satish84s.site"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z10145872BEYXWGCKYUZ9"
+  description = "description"
+}
